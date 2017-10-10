e9e13f30c2c852077b7e4c376e343780365ac0b8
Michael Dennison <MikeDennison@Michaels-iMac.local>
2017-10-10 11:19:05 -0700

    backup of SCSS files

--- wp-content/themes/c4c/archive-conversations.php
+++ wp-content/themes/c4c/archive-conversations.php
@@ -40,7 +40,7 @@
 
 								</div>
 								<div class="convo-search">
-									<form id="searchform" action="http://localhost:8888/c4c" method="get">
+									<form id="searchform" action="http://challengeforchange.ca/" method="get"> 
 									        <input class="inlineSearch" type="text" name="s" value="Search Conversations" onblur="if (this.value == '') {this.value = 'Search Conversations';}" onfocus="if (this.value == 'Search Conversations') {this.value = '';}" />
 									        <input type="hidden" name="post_type" value="conversations" />
 									        <input class="inlineSubmit aqua-btn sm inline" id="searchsubmit" type="submit" alt="Search" value="Search" />
